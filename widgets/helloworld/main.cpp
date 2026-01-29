27ba87c43fd37c6a088621ef4fcd599bef664512
DonizeteVida <donizetevida1998@outlook.com>
2026-01-29 23:50:31 +0100

    Move widget helloworld example

---
+++ widgets/helloworld/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <QtWidgets>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.resize(320, 240);
+    window.show();
+    window.setWindowTitle(QApplication::translate("toplevel", "Top-level widget"));
+
+    QPushButton *button = new QPushButton(
+        QApplication::translate("childwidget", "Press me"),
+        &window);
+    button->move(100, 100);
+    button->show();
+
+    return app.exec();
+}
